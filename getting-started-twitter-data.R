fad1904a2cca684bb25bc409eec0582a7df11902
Leo Brown <leo.e.brown@gmail.com>
2016-04-11 12:55:58 -0400

    sharing twitter api getting started

--- getting-started-twitter-data.R
+++ getting-started-twitter-data.R
@@ -1,3 +1,5 @@
+#https://cran.r-project.org/web/packages/twitteR/twitteR.pdf
+
 library(ROAuth)
 library(streamR)
 library(devtools)
@@ -12,6 +14,11 @@ api_secret <- 'iJBUs2X40D3KKnM1pveTRvpDz5WAYFyrTJ2LP9Qbj0vzcGLhP8'
 access_token <- '219841618-9HSysAb8iZR0mw9G76bCjMuL70hTCqnw90m9LkxS'
 access_token_secret <- 'T3OSXcuzozq4Jd3Y6ISEJZtYJBIU8FY5PgzbUver5GotT'
 
+df <- searchTwitter("megabus,late",n=50)
+View(twListToDF(df))
+
+
+
 #The following (commented out) code is what Leo used to set up OAuth credentials. I don't think it's necessary to ever run this again.
 #credential <- OAuthFactory$new(consumerKey=api_key,
 #                               consumerSecret=api_secret,
@@ -21,5 +28,4 @@ access_token_secret <- 'T3OSXcuzozq4Jd3Y6ISEJZtYJBIU8FY5PgzbUver5GotT'
 #options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
 #download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
 #credential$handshake(cainfo="cacert.pem")
-setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 
