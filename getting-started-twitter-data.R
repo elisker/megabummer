082f827fc8a88e8f41d9aa8dbfff46e55aad176a
Leo Brown <leo.e.brown@gmail.com>
2016-04-20 11:01:13 -0400

    two versions of twitter_oauth for two different .Rprofile setups

--- getting-started-twitter-data.R
+++ getting-started-twitter-data.R
@@ -1,5 +1,14 @@
 #Documentation for twitteR: https://cran.r-project.org/web/packages/twitteR/twitteR.pdf
 
+
+#SETTING AN .RPROFILE
+#1. Figure out what your home directory is
+path.expand("~")
+#2. File > New File > R Script
+#3. Save as .Rprofile in directory indicated in step 1.
+#4. Declare variables for your keys, etc.
+
+
 #library(ROAuth)
 #library(streamR)
 #library(devtools)
@@ -10,6 +19,12 @@ library(twitteR)
 
 
 setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+#for leo:
+setup_twitter_oauth(Sys.getenv("api_key"),
+                    Sys.getenv("api_secret"),
+                    Sys.getenv("access_token"),
+                    Sys.getenv("access_token_secret"))
+
 
 df <- searchTwitter("megabus,late",n=50)
 View(twListToDF(df))
