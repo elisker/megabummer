640457982d7de27e94b918165cb25f2b3cadc5a9
Emily B Lisker <eblisker@emilys-mbp-2.public.wireless.med.harvard.edu>
2016-04-26 11:23:50 -0400

    Added code for using megabus-specific lexicon. Still need to build out lexicon.

--- megabummer_david.R
+++ megabummer_david.R
@@ -15,6 +15,12 @@ library(dplyr)
 #         access_token = "BLABLABLA",
 #         access_token_secret = "BLABLABLA")
 
+# editing .Rprofile
+# file.edit(".RProfile")
+
+# restart R within Rstudio 
+# .rs.restartR()
+
 #The following (commented out) code is what Leo used to set up OAuth credentials. I don't think it's necessary to ever run these again.
 #install.packages("ROauth")
 #install.packages("streamR")
@@ -34,6 +40,7 @@ library(dplyr)
 #setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 
 # setup for Emily
+setwd(dir = "/Users/eblisker/Documents/HSPH/Courses/2016 Spring/BIO 260/Final/megabummer")
 setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 
 # searching Twitter
@@ -46,6 +53,30 @@ library(devtools)
 library(tidytext)
 # other text mining: tm, quanteda
 
+#' Tidytext contains three lexicons for sentiment analysis are combined here in a tidy data frame.
+#' The lexicons are the NRC Emotion Lexicon from Saif Mohammad and Peter Turney,
+#' the sentiment lexicon from Bing Liu and collaborators, and the lexicon of
+#' Finn Arup Nielsen. Words with non-ASCII characters were removed from the
+#' lexicons.
+
+#' @format A data frame with 23,165 rows and 4 variables:
+#' \describe{
+#'  \item{word}{An English word}
+#'  \item{sentiment}{One of either positive, negative, anger, anticipation,
+#'  disgust, fear, joy, sadness, surprise, trust, or \code{NA}. The Bing lexicon
+#'  has positive/negative, the NRC lexicon has all options except \code{NA}, and
+#'  the AFINN lexicon has only \code{NA}.}
+#'  \item{lexicon}{The source of the sentiment for the word. One of either
+#'  "nrc", "bing", or "AFINN".}
+#'  \item{score}{A numerical score for the sentiment. This value is \code{NA}
+#'  for the Bing and NRC lexicons, and runs between -5 and 5 for the AFINN
+#'  lexicon.}
+#' }
+#'
+#' @source \url{http://saifmohammad.com/WebPages/lexicons.html}
+#' \url{https://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html}
+#' \url{http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=6010}
+
 by_word <- tweets_df %>%
   filter(!isRetweet) %>%
   select(text, id, created) %>%
@@ -54,16 +85,33 @@ by_word <- tweets_df %>%
 by_word %>%
   count(word, sort = TRUE)
 
+# original code from David
+
+#bing <- sentiments %>%
+#  filter(lexicon == "bing") %>%
+#  select(-score)
+
+# Supplemented bing sentiment lexicon with megabus specific sentiment. These additional negative and positive words
+# were identified  by manually reviewing 2900 tweets on megabus queried on 4/20/2016. Negative and positive words not already
+# included in the bing lexicon, such as those related to megabus and or transportation experience specifically, were added to the 
+# lexicon.
+
 library(tidyr)
-bing <- sentiments %>%
-  filter(lexicon == "bing") %>%
+library(readr)
+megabus_lexicon <- read_csv("megabus_lexicon.csv")
+
+# create new dataframe of bing and megabummer sentiments
+bing_megabus <- megabus_lexicon %>%
+  filter(lexicon %in% c("bing","megabummer")) %>%
   select(-score)
 
+# create new dataframe calculating megabus sentiment
 megabussentiment <- by_word %>%
-  inner_join(bing) %>% 
+  inner_join(bing_megabus) %>% 
   count(word, sentiment) %>% 
   spread(sentiment, n, fill = 0) %>% 
   mutate(sentiment = positive - negative)
 
 # Export file as csv
-write.csv(by_word, file = "megabus_4-20.csv")
+write.csv(tweets_df, file = "megabus_tweets_df_4-26.csv")
+write.csv(by_word, file = "megabus_by_word_4-26.csv")
